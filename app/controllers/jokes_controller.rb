9be2efadcd01ee24a1573b72049e2a2911ca52df
kaizokush <ushakya3@gmail.com>
2016-08-19 18:55:50 +0545

    edit works

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -2,15 +2,15 @@ class JokesController < ApplicationController
 	def index
 		@jokes=Joke.all		
 	end
+	def edit
+  @joke = Joke.find(params[:id])
+end
 	def create
 		@joke=Joke.create(joke_params)
 		redirect_to root_path
 	end
-	private
-	def joke_params
-		params.require(:joke).permit(:jokes,:author)
-	end
-	def update
+		
+		def update
  	 @joke = Joke.find(params[:id])
   	if @joke.update(joke_params)
     redirect_to root_path
@@ -23,4 +23,10 @@ class JokesController < ApplicationController
   		@joke.destroy
   		redirect_to root_path
 	end
+	
+	private
+	def joke_params
+		params.require(:joke).permit(:jokes,:author)
+	end
+
 end
