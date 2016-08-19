c39788ed0fc89cd60d361ca592497bc978e76f3b
kaizokush <ushakya3@gmail.com>
2016-08-19 17:33:48 +0545

    Form to add jokes

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'simple_form'
 gem 'font-awesome-sass'
 gem 'bootstrap-sass', '~> 3.3.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -2,4 +2,12 @@ class JokesController < ApplicationController
 	def index
 		@jokes=Joke.all		
 	end
+	def create
+		@joke=Joke.create(joke_params)
+		redirect_to root_path
+	end
+	private
+	def idea_params
+		params.require(:joke).permit(:jokes,:author)
+	end
 end
